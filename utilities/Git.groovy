551f5bdf337b7f28f343339939be85ded62fb07d
anhlk <leeka2201@gmail.com>
2024-07-16 08:57:05 +0000

    update groovy script to get list of git branches

--- utilities/Git.groovy
+++ utilities/Git.groovy
@@ -6,32 +6,32 @@ class Git {
         String credentialsId
     ) {
         return """
-        import com.cloudbees.plugins.credentials.CredentialsMatchers
-        import com.cloudbees.plugins.credentials.CredentialsProvider
-        import com.cloudbees.plugins.credentials.common.StandardUsernameCredentials
-        import com.cloudbees.plugins.credentials.domains.DomainRequirement
-        import jenkins.model.Jenkins
-        import hudson.security.ACL
+import com.cloudbees.plugins.credentials.CredentialsMatchers
+import com.cloudbees.plugins.credentials.CredentialsProvider
+import com.cloudbees.plugins.credentials.common.StandardUsernameCredentials
+import com.cloudbees.plugins.credentials.domains.DomainRequirement
+import jenkins.model.Jenkins
+import hudson.security.ACL
 
-        def jenkins = Jenkins.get()
-        def lookupSystemCredentials = { credentialsId ->
-        return CredentialsMatchers.firstOrNull(
-            CredentialsProvider
-            .lookupCredentials(StandardUsernameCredentials.class, jenkins, ACL.SYSTEM,
-            Collections.<DomainRequirement>emptyList()),
-            CredentialsMatchers.withId(credentialsId)
-        )
-        }
+def jenkins = Jenkins.get()
+def lookupSystemCredentials = { credentialsId ->
+return CredentialsMatchers.firstOrNull(
+    CredentialsProvider
+    .lookupCredentials(StandardUsernameCredentials.class, jenkins, ACL.SYSTEM,
+    Collections.<DomainRequirement>emptyList()),
+    CredentialsMatchers.withId(credentialsId)
+)
+}
 
-        def credential = lookupSystemCredentials('$credentialsId')
-        def user = credential.getUsername()
-        def accessToken = credential.getPassword().getPlainText()
-        def repository = '$repository'
+def credential = lookupSystemCredentials('$credentialsId')
+def user = credential.getUsername()
+def accessToken = credential.getPassword().getPlainText()
+def repository = '$repository'
 
-        def gitUrl = "https://oauth2:\${accessToken}@github.com/\${user}/\${repository}"
-        def gitBranches = "git ls-remote --heads \${gitUrl}".execute().text.readLines().collect { it.split()[1].replaceAll("refs/heads/", "") }.sort().reverse()
+def gitUrl = "https://oauth2:\${accessToken}@github.com/\${user}/\${repository}"
+def gitBranches = "git ls-remote --heads \${gitUrl}".execute().text.readLines().collect { it.split()[1].replaceAll("refs/heads/", "") }.sort().reverse()
 
-        return gitBranches
-        """
+return gitBranches
+"""
     }
 }
